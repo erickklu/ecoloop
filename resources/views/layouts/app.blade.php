fc41d70cba238ca03a225528339a1e1e2c813019
Mario Moreno <mario_morenor@hotmail.com>
2024-06-16 12:05:43 -0500

    Fix. Removido CSS innecesario de Layout View

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
-    @vite(['resources/css/app.css','resources/sass/app.scss'])
+    @vite(['resources/sass/app.scss'])
 
     <title>@yield("title")</title>
     
