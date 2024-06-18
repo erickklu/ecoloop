1c3e41dba3f2617eb441b9a76d3902f50bb75ae0
Jonathan Moreno <jmariomorenor@gmail.com>
2024-06-18 08:32:35 -0500

    Update.
    Se ha agregado acciones para accesos directos dentro de las peticiones.

---
+++ app/VoyagerActions/RequestedEntryAction.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\VoyagerActions;
+
+use TCG\Voyager\Actions\AbstractAction;
+
+class RequestedEntryAction extends AbstractAction
+{
+
+    public function getTitle()
+    {
+        return ' Ver Publicación';
+    }
+
+    public function getIcon()
+    {
+        return 'voyager-file-text';
+    }
+
+    public function getPolicy()
+    {
+        return;
+    }
+
+    public function getAttributes()
+    {
+        return [
+            'class' => 'btn btn-sm btn-success',
+        ];
+    }
+
+    public function getDefaultRoute()
+    {
+        return route('publicaciones.detalle', ["id" => $this->data->id]);
+    }
+
+    public function shouldActionDisplayOnDataType()
+    {
+        return $this->dataType->slug == 'requested_entries';
+    }
+}
+
+
+class RequestedEntryUserAction extends AbstractAction
+{
+
+    public function getTitle()
+    {
+        return ' Ver Usuario';
+    }
+
+    public function getIcon()
+    {
+        return 'voyager-person';
+    }
+
+    public function getPolicy()
+    {
+        return;
+    }
+
+    public function getAttributes()
+    {
+        return [
+            'class' => 'btn btn-sm btn-primary',
+        ];
+    }
+
+    public function getDefaultRoute()
+    {
+        return route('perfil',["id"=>$this->data->user_id]);
+    }
+
+    public function shouldActionDisplayOnDataType()
+    {
+        return $this->dataType->slug == 'requested_entries';
+    }
+}
