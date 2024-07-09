19cc8d7d2be612d5b74c288808dfbcc05b60b6ea
erick <ericktrejojohanlucio@gmail.com>
2024-07-09 01:53:07 -0500

    Changues migrations whatsapp

--- database/migrations/2024_07_09_015057_users_add_whatsapp.php
+++ database/migrations/2024_07_09_015057_users_add_whatsapp.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('whatsapp')->default('')->change();
+            $table->string('whatsapp');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('whatsapp')->default(null)->change();
+            $table->dropColumn('whatsapp');
         });
     }
 };
