49b658343ceba7337d0207ce409528394057d122
erick <ericktrejojohanlucio@gmail.com>
2024-07-12 20:12:49 -0500

    Edit footer del panel

--- resources/views/vendor/voyager/partials/app-footer.blade.php
+++ resources/views/vendor/voyager/partials/app-footer.blade.php
@@ -3,7 +3,7 @@
         @if (rand(1,100) == 100)
             <i class="voyager-rum-1"></i> {{ __('voyager::theme.footer_copyright2') }}
         @else
-            {!! __('voyager::theme.footer_copyright') !!} <a href="http://thecontrolgroup.com" target="_blank">Erick Trejo y Ricardo Arias</a>
+            {!! __('voyager::theme.footer_copyright') !!} <a target="_blank">Erick Trejo y Ricardo Arias</a>
         @endif
         @php $version = Voyager::getVersion(); @endphp
         @if (!empty($version))
